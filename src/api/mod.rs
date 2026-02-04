4013b30998092086f8108b699536af525fcb76b9
Roc Sun <41471196+RocsSun@users.noreply.github.com>
2026-02-04 21:22:19 +0800

    refactor: restructure code and CLI commands (#7)

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,33 +1,14 @@
-pub mod auth;
+//!
+//! API
+//!
+//! 负责HTTP的调用，返回对应的response
+//!
+
 pub mod fav;
 pub mod ing;
 pub mod news;
 pub mod post;
+pub mod urls;
 pub mod user;
 
-pub const BLOG_BACKEND: &str = "https://i.cnblogs.com/api";
-#[macro_export]
-macro_rules! blog_backend {
-    ($($arg:tt)*) => {{
-        use $crate::api::BLOG_BACKEND;
-        format!("{}{}", BLOG_BACKEND, format_args!($($arg)*))
-    }};
-}
-
-pub const OPENAPI: &str = "https://api.cnblogs.com/api";
-#[macro_export]
-macro_rules! openapi {
-    ($($arg:tt)*) => {{
-        use $crate::api::OPENAPI;
-        format!("{}{}", OPENAPI, format_args!($($arg)*))
-    }};
-}
-
-pub const OAUTH: &str = "https://oauth.cnblogs.com";
-#[macro_export]
-macro_rules! oauth {
-    ($($arg:tt)*) => {{
-        use $crate::api::OAUTH;
-        format!("{}{}", OAUTH, format_args!($($arg)*))
-    }};
-}
+pub struct Client {}
